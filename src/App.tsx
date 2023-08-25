38ad342913ab375365edf31ec199805d99becd7c
Zacharia <ndunguzachary24@gmail.com>
2023-08-25 09:51:44 +0300

    Added formik and react hot toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 
-
+import {Toaster} from 'react-hot-toast'
 
 function App() {
   return (
+    <div>
+      <Toaster position='bottom-center'/>
     <h1 className="text-5xl text-center font-bold underline">
     Hello world!
   </h1>
+    </div>
+
   )
 }
 
---
+++ src/Components/AddTodo.tsx
@@ -0,0 +1,13 @@
+import { Formik } from 'formik'
+
+const AddTodo = () => {
+    return (
+      <div>
+            <Formik
+            initialValues={{}}>
+            </Formik>
+            </div>
+  )
+}
+
+export default AddTodo
